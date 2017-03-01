8fd37e80f51e8b0a7251c3b8a6b2d339f024a611
Joshua Rinard and Marty Kovach <joshua+marty@>
2017-02-28 16:57:44 -0800

    api gets task and gets boxes

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,7 +5,6 @@ class Picture < ApplicationRecord
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-
 # before_create :get_task
  after_save :get_task
   def get_task
@@ -31,7 +30,6 @@ class Picture < ApplicationRecord
   def get_details
 
     begin
-      
     response = RestClient::Request.new(
       :method => :get,
       :url => "https://api.deepomatic.com/v0.6/tasks/#{self.task_id}/",
@@ -46,36 +44,4 @@ class Picture < ApplicationRecord
     end
     JSON.parse(response)
   end
-
-
-
-
 end
-#
-# ap id    413134115772
-# api key  a521ecad9baa4eadad13c82f56d0d9ff
-#
-# https://api.deepomatic.com/v0.6/tasks/269751802/
-#
-# https://api.deepomatic.com/v0.6/detect/weapons/?url=http://www.kimberamerica.com/media/catalog/category/pistols-hero.png
-#
-#
-
-
-#
-# begin
-# response = RestClient::Request.new(
-#   :method => :post,
-#   :url => 'https://api.twilio.com/2010-04-01/Accounts/AC92343d189fc553f58a92f9895da8ced5/Messages.json',
-#   :user => ENV['Twilio_Acc_Sid'],
-#   :password => ENV['Twilio_Auth_Token'],
-#   :payload => { :Body => body,
-#                 :From => from,
-#                 :To => to}
-# ).execute
-# rescue RestClient::BadRequest => error
-#   message = JSON.parse(error.response)['message']
-#   errors.add(:base, message)
-#   throw(:abort) #helping it not crash if it fails to save
-# # end
-# end
